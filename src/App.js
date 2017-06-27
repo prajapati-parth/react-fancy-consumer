729a050e18ae2e0b95c5c525bfe4c22a72b0a0f7
prajapati-parth <parth17prajapati@gmail.com>
2017-06-27 10:00:09 +0530

    Consumed TodoList component from #2415242

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Fancy from 'react-fancy-component'
+import React, { Component } from 'react'
+import './App.css'
+import { Fancy } from 'react-fancy-component'
+import { TodoList } from 'react-fancy-component'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        This is our Fancy Component: <Fancy />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div className="App-header">
+					<h2>React Todo</h2>
+				</div>
+				<div className="bodyContainer">
+					<Fancy
+						textLog={this.handleOnClick.bind(this)}
+					/>
+					<h3>Current todo list</h3>
+					<TodoList />
+				</div>
+			</div>
+		);
+	}
+
+	handleOnClick() {
+		console.log("clicked!")
+	}
 }
 
 export default App;
